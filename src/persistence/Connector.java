511f16711f7d1d11a0404f8de54e4e49bf4bcbc9
AngelIBM <testing.ibm.ios@gmail.com>
2016-04-28 13:11:46 -0500

    Conector Correcto

--- src/persistence/Connector.java
+++ src/persistence/Connector.java
@@ -11,7 +11,7 @@ public class Connector {
 	public Connection CreateConnection(){
 		try{
 			Class.forName("com.ibm.db2.jcc.DB2Driver");
-			String url = "jdbc:db2://169.54.229.15:50000/s0447899:clientRerouteAlternateServerName=169.55.248.232;client";
+			String url = "jdbc:db2://169.54.229.15:50000/S0447899:clientRerouteAlternateServerName=169.55.248.232;clientRerouteAlternatePortNumber=50000;";
 			String user = "ininjfpv";
 			String password = "nh1xy6imjjoe";
 			connection= DriverManager.getConnection(url, user, password); 
